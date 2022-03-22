f5b4eaee3c09a7ad53f2ea14047ddf1f0d952dd4
Leonid Dubinsky <dub@podval.org>
2022-03-22 15:45:38 -0400

    - cleanup;

--- README.md
+++ README.md
@@ -17,8 +17,13 @@ and include it in the compound build: ... .
 
 ...
 
-## Possible Enhancements ##
+## Limitations and Possible Enhancements ##
 
+- add ability to configure a list of entry points;
+- are exports enough to produce JavaScript for multiple games?
+- how to cross-compile using shared/jvm/js?
+  ([the way sbt does it](https://www.scala-js.org/doc/project/cross-build.html))
+- discover the main class
 - discover the version of ScalaJS from the classpath and use reflection
 to match the version of the linker
 - discover the version of Scala from the classpath and configure
@@ -31,3 +36,15 @@ to match the version of the linker
 Stack Overflow [answer](https://stackoverflow.com/a/65777102/670095)
 by [gzm0](https://stackoverflow.com/users/1149944/gzm0) was
 *extremely* helpful. Thanks!
+
+[ScalaJS sbt plugin](https://github.com/scala-js/scala-js/tree/main/sbt-plugin/src/main/scala/org/scalajs/sbtplugin)
+
+[Tutorial](https://www.scala-js.org/doc/tutorial/basic/)
+
+[Scala JS CLI](https://github.com/scala-js/scala-js-cli/tree/main/src/main/scala/org/scalajs/cli)
+
+ScalaJS Linker
+
+[IMPLEMENTING SCALA.JS SUPPORT FOR SCALA 3](https://www.scala-lang.org/2020/11/03/scalajs-for-scala-3.html)
+
+[The old plugin](https://github.com/gtache/scalajs-gradle)
--- src/main/scala/org/podval/tools/scalajs/ScalaJSPlugin.scala
+++ src/main/scala/org/podval/tools/scalajs/ScalaJSPlugin.scala
@@ -2,16 +2,17 @@ package org.podval.tools.scalajs
 
 import org.gradle.api.{DefaultTask, Plugin, Project}
 import org.gradle.api.file.FileCollection
+import org.gradle.api.logging.LogLevel
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.{SourceSet, TaskAction}
 import org.scalajs.linker.{PathIRContainer, PathOutputDirectory, StandardImpl}
-import org.scalajs.linker.interface.{IRContainer, IRFile, IRFileCache, Linker, ModuleInitializer, ModuleKind, ModuleSplitStyle, OutputDirectory, Report, Semantics, StandardConfig}
-import org.scalajs.logging.{Level, Logger, ScalaConsoleLogger}
+import org.scalajs.linker.interface.{IRContainer, IRFile, ModuleInitializer, ModuleKind, ModuleSplitStyle, Report,
+  Semantics, StandardConfig}
+import org.scalajs.logging.{Level, Logger}
 
 import java.io.File
-import java.nio.file.Path
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.jdk.CollectionConverters.*
@@ -23,98 +24,116 @@ final class ScalaJSPlugin extends Plugin[Project]:
     project.getTasks.create("fullLinkJS", classOf[ScalaJSPlugin.FullLinkTask])
 
 object ScalaJSPlugin:
+
   abstract class Extension:
     def getMainClass: Property[String]
     def getMainMethod: Property[String]
     def getMainMethodHasArgs: Property[Boolean]
 
-    final def getMainModuleInitializer: ModuleInitializer =
-      val mainClass : String = getOrElse(getMainClass , throw new IllegalArgumentException("'scalajs.mainClass' is not set"))
+    final def mainModuleInitializer: Option[ModuleInitializer] = optional(getMainClass).map { (mainClass: String) =>
       val mainMethod: String = getOrElse(getMainMethod, "main")
-      if getOrElse(getMainMethodHasArgs, true)
+      if getOrElse(getMainMethodHasArgs, false)
       then ModuleInitializer.mainMethodWithArgs(mainClass, mainMethod)
-      else ModuleInitializer.mainMethod        (mainClass, mainMethod)
+      else ModuleInitializer.mainMethod(mainClass, mainMethod)
+    }
+
+    def getOutputDirectory: Property[File]
+    final def outputDirectory(project: Project): File = getOrElse(getOutputDirectory, File(project.getBuildDir, "js"))
 
     def getModuleKind: Property[String]
-
-    final def moduleKind: ModuleKind = byName(getModuleKind, ModuleKind.NoModule, ModuleKind.All)
-
     def getModuleSplitStyle: Property[String]
+    def getPrettyPrint: Property[Boolean]
+    // TODO more properties for the other withXXX()
 
-    final def moduleSplitStyle: ModuleSplitStyle = byName(getModuleSplitStyle, ModuleSplitStyle.FewestModules,
-      List(ModuleSplitStyle.FewestModules, ModuleSplitStyle.SmallestModules))
+    private val moduleSplitStyles: List[ModuleSplitStyle] = List(ModuleSplitStyle.FewestModules, ModuleSplitStyle.SmallestModules)
 
-  // TODO
-//    optimizer = true,
-//    jsHeader = "",
-//    parallel = true,
-//    sourceMap = true,
-//    relativizeSourceMapBase = None,
-//    closureCompilerIfAvailable = false,
-//    prettyPrint = false,
-//    batchMode = false,
-//    maxConcurrentWrites = 50
+    final def linkerConfig(fullOptimization: Boolean): StandardConfig =
+      val moduleKind: ModuleKind = byName(getModuleKind, ModuleKind.NoModule, ModuleKind.All)
+      StandardConfig()
+        .withCheckIR(fullOptimization)
+        .withSemantics(if fullOptimization then Semantics.Defaults.optimized else Semantics.Defaults)
+        .withClosureCompiler(fullOptimization && (moduleKind != ModuleKind.ESModule))
+        .withModuleKind(moduleKind)
+        .withModuleSplitStyle(byName(getModuleSplitStyle, ModuleSplitStyle.FewestModules, moduleSplitStyles))
+        .withPrettyPrint(getOrElse(getPrettyPrint, false))
+
+  class FastLinkTask extends LinkTask(fullOptimization = false, descriptionSuffix = "fast"             )
+  class FullLinkTask extends LinkTask(fullOptimization = true , descriptionSuffix = "full optimization")
+
+  abstract class LinkTask(fullOptimization: Boolean, descriptionSuffix: String) extends DefaultTask:
+    setGroup("build")
+    setDescription(s"Link ScalaJS - $descriptionSuffix")
+
+    private def getExtension: Extension = getProject.getExtensions.getByType(classOf[Extension])
+    private def getRuntimeClassPath: FileCollection = getMainSourceSet(getProject).getRuntimeClasspath
+    private def info(message: String): Unit = getProject.getLogger.info(message, null, null, null)
+
+    getProject.afterEvaluate { (project: Project) =>
+      getDependsOn.add(getClassesTask(project))
+      getInputs.files(getRuntimeClassPath)
+      getOutputs.dir(getExtension.outputDirectory(project))
+      () // return Unit to help the compiler find the correct overload
+    }
+
+    @TaskAction def execute(): Unit =
+      val extension: Extension = getExtension
+      val outputDirectory: File = extension.outputDirectory(getProject)
+
+      // Without the initializer, no JavaScript is emitted!
+      val mainModuleInitializer: Option[ModuleInitializer] = extension.mainModuleInitializer
+      val linkerConfig: StandardConfig = extension.linkerConfig(fullOptimization)
+
+      info(
+        s"""ScalaJSPlugin:
+           |outputDirectory = $outputDirectory
+           |mainModuleInitializer = ${mainModuleInitializer.map(ModuleInitializer.fingerprint)}
+           |linkerConfig = $linkerConfig
+           |""".stripMargin,
+      )
+
+      outputDirectory.mkdirs()
+
+      val report: Report = Await.result(atMost = Duration.Inf, awaitable = PathIRContainer
+        .fromClasspath(getRuntimeClassPath.getFiles.asScala.toSeq.map(_.toPath))
+        .map(_._1)
+        .flatMap((irContainers: Seq[IRContainer]) => StandardImpl.irFileCache().newCache.cached(irContainers))
+        .flatMap((irFiles     : Seq[IRFile]     ) => StandardImpl.linker(linkerConfig).link(
+          irFiles = irFiles,
+          moduleInitializers = mainModuleInitializer.toSeq,
+          output = PathOutputDirectory(outputDirectory.toPath),
+          logger = GradleLogger(getProject)
+        ))
+      )
+
+      info(report.toString)
+
+  private def getMainSourceSet(project: Project): SourceSet = project
+    .getConvention
+    .findPlugin(classOf[JavaPluginConvention])
+    .getSourceSets
+    .getByName(SourceSet.MAIN_SOURCE_SET_NAME)
+
+  private def getClassesTask(project: Project) = project
+    .getTasks
+    .findByName(getMainSourceSet(project).getClassesTaskName)
+
+  private def optional[T](property: Property[T]): Option[T] =
+    if !property.isPresent then None else Some(property.get)
 
   private def getOrElse[T](property: Property[T], default: => T): T =
     if !property.isPresent then default else property.get
 
-  private def byName[T](property: Property[String], default: T, all: List[T]): T =
-    val name: String = getOrElse(property, default.toString)
-    all.find(_.toString == name).get
+  private def byName[T](property: Property[String], default: => T, all: List[T]): T =
+    if !property.isPresent then default else all.find(_.toString == property.get).get
 
-  class FastLinkTask extends DefaultTask:
-    setGroup("build")
-    setDescription("Link ScalaJs - fast")
-    @TaskAction def execute(): Unit = link(getProject, fullOptimization = false)
+  final class GradleLogger(project: Project) extends Logger:
+    override def trace(t: => Throwable): Unit =
+      project.getLogger.error("ScalaJS Linker Error", t)
+    override def log(level: Level, message: => String): Unit =
+      project.getLogger.log(scalajs2gradleLevel(level), "ScalaJS Linker: " + message)
 
-  class FullLinkTask extends DefaultTask:
-    setGroup("build")
-    setDescription("Link ScalaJs - full optimization")
-    @TaskAction def execute(): Unit = link(getProject, fullOptimization = true)
-
-  private def link(project: Project, fullOptimization: Boolean): Unit =
-    val extension: Extension = project.getExtensions.getByType(classOf[Extension])
-
-    val outputDir: File = File(project.getBuildDir, "js")
-    outputDir.mkdirs()
-
-    val linkerConfig: StandardConfig = StandardConfig()
-      .withCheckIR  (fullOptimization)
-      .withSemantics((semantics: Semantics) => if fullOptimization then semantics.optimized else semantics)
-      .withClosureCompiler(fullOptimization && extension.moduleKind != ModuleKind.ESModule)
-
-      .withModuleKind      (extension.moduleKind)
-      .withModuleSplitStyle(extension.moduleSplitStyle)
-
-    // Same as scalaJSModuleInitializers in sbt, add if needed.
-    // Without the initializer, no JavaScript is emitted!
-    val moduleInitializers: Seq[ModuleInitializer] = Seq(
-      extension.getMainModuleInitializer
-    )
-
-    val report: Report = Await.result(atMost = Duration.Inf, awaitable = PathIRContainer
-      .fromClasspath(getMainSourceSet(project).getRuntimeClasspath.getFiles.asScala.toSeq.map(_.toPath))
-      .map(_._1)
-      .flatMap((irContainers: Seq[IRContainer]) => StandardImpl.irFileCache().newCache.cached(irContainers))
-      .flatMap((irFiles     : Seq[IRFile]     ) => StandardImpl.linker(linkerConfig).link(
-        irFiles,
-        moduleInitializers,
-        PathOutputDirectory(outputDir.toPath),
-        ScalaConsoleLogger(Level.Debug) // TODO GradleLogger(logger)
-      ))
-    )
-
-  private def getMainSourceSet(project: Project): SourceSet =
-    Option(project.getConvention.findPlugin(classOf[JavaPluginConvention]))
-      .get
-      .getSourceSets
-      .getByName(SourceSet.MAIN_SOURCE_SET_NAME)
-
-  // TODO use this after correcting the levels
-  final class GradleLogger(logger: org.gradle.api.logging.Logger) extends Logger:
-    override def trace(t: => Throwable): Unit = logger.error("ScalaJS Link Error", t)
-    override def log(level: Level, message: => String): Unit = level match
-      case Level.Warn  => logger.warn (message)
-      case Level.Error => logger.error(message)
-      case Level.Info  => logger.info (message, null, null, null)
-      case Level.Debug => logger.debug(message, null, null, null)
+  private def scalajs2gradleLevel(level: Level): LogLevel = level match
+    case Level.Error => LogLevel.ERROR
+    case Level.Warn  => LogLevel.WARN
+    case Level.Info  => LogLevel.INFO
+    case Level.Debug => LogLevel.DEBUG
