b956f34676b2d70de0bea1680da8a3c7e78979a1
Leonid Dubinsky <dub@podval.org>
2025-04-01 01:22:21 -0400

    - removing no longer needed serialization (TaskDefs, Selectors, Strings, Fingerprints);

--- src/main/scala/org/podval/tools/test/taskdef/Strings.scala
+++
@@ -1,8 +0,0 @@
-package org.podval.tools.test.taskdef
-
-object Strings extends Ops[String]("<never needed>"):
-  object Many extends ArrayOps[String](Strings, "///")
-
-  override protected def toStrings(value: String): Array[String] = Array(value)
-  override protected def fromStrings(strings: Array[String]): String = strings(0)
-  
