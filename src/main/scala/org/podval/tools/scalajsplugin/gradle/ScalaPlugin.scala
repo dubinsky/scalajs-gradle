9232b6d5bbc165e4b7f1660bb0c0d28d99a22a28
Leonid Dubinsky <dub@podval.org>
2025-04-06 00:07:25 -0400

    - WIP: mixed projects;

--- src/main/scala/org/podval/tools/scalajsplugin/gradle/ScalaPlugin.scala
+++ src/main/scala/org/podval/tools/scalajsplugin/gradle/ScalaPlugin.scala
@@ -3,29 +3,43 @@ package org.podval.tools.scalajsplugin.gradle
 import org.gradle.api.{Action, Project}
 import org.gradle.api.artifacts.{ConfigurablePublishArtifact, Configuration}
 import org.gradle.api.file.{ConfigurableFileCollection, RegularFile}
-import org.gradle.api.plugins.{JavaBasePlugin, JavaPlugin}
 import org.gradle.api.plugins.internal.JavaPluginHelper
-import org.gradle.api.plugins.jvm.internal.JvmFeatureInternal
-import org.gradle.api.plugins.scala.ScalaBasePlugin
+import org.gradle.api.plugins.jvm.internal.{JvmFeatureInternal, JvmPluginServices}
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.{ScalaSourceDirectorySet, TaskProvider}
 import org.gradle.api.tasks.scala.ScalaDoc
 import org.gradle.language.scala.tasks.AbstractScalaCompile
+import org.podval.tools.scalajsplugin.GradleNames
 
 // see org.gradle.api.plugins.scala.ScalaPlugin
 import org.gradle.api.plugins.scala.ScalaPlugin as Original
 
-abstract class ScalaPlugin:
-  def apply(project: Project): Unit =
-    project.getPluginManager.apply(classOf[ScalaBasePlugin])
-    project.getPluginManager.apply(classOf[JavaPlugin])
+final class ScalaPlugin(
+  isCreate: Boolean,
+  sourceRoot: String,
+  sharedSourceRoot: String,
+  gradleNames: GradleNames,
+  project: Project,
+  jvmPluginServices: JvmPluginServices // TODO get from the project?
+):
+  def apply(): Unit =
+    ScalaBasePlugin(
+      isCreate, 
+      sourceRoot,
+      sharedSourceRoot,
+      gradleNames,
+      project,
+      jvmPluginServices
+    ).apply()
+    
+    // TODO JavaPlugin().apply()
 
     val mainFeature: JvmFeatureInternal = JavaPluginHelper.getJavaComponent(project).getMainFeature
 
-    ScalaPlugin.configureScaladoc(project, mainFeature)
+    configureScaladoc(mainFeature)
 
     val incrementalAnalysisElements: Configuration = project.getConfigurations.getByName("incrementalScalaAnalysisElements")
-    val compileTaskName: String = mainFeature.getSourceSet.getCompileTaskName("scala")
+    val compileTaskName: String = mainFeature.getSourceSet.getCompileTaskName(gradleNames.scalaCompileTaskName)
     val compileScala: TaskProvider[AbstractScalaCompile] = project.getTasks.withType(classOf[AbstractScalaCompile]).named(compileTaskName)
     val compileScalaMapping: Provider[RegularFile] = project.getLayout.getBuildDirectory.file(s"tmp/scala/compilerAnalysis/$compileTaskName.mapping")
     compileScala.configure(task => task.getAnalysisMappingFile.set(compileScalaMapping))
@@ -35,8 +49,8 @@ abstract class ScalaPlugin:
         override def execute(configurablePublishArtifact: ConfigurablePublishArtifact): Unit = configurablePublishArtifact.builtBy(compileScala)
     )
 
-object ScalaPlugin:
-  private def configureScaladoc(project: Project, feature: JvmFeatureInternal): Unit =
+  private def configureScaladoc(feature: JvmFeatureInternal): Unit =
+    // TODO account for multiple pairs of main/test source sets
     project.getTasks.withType(classOf[ScalaDoc]).configureEach(scalaDoc =>
       scalaDoc.getConventionMapping.map("classpath", () =>
         val files: ConfigurableFileCollection = project.files()
@@ -47,12 +61,13 @@ object ScalaPlugin:
       scalaDoc.setSource(feature.getSourceSet.getExtensions.getByType(classOf[ScalaSourceDirectorySet]))
       scalaDoc.getCompilationOutputs.from(feature.getSourceSet.getOutput)
     )
-    project.getTasks.register(
-      Original.SCALA_DOC_TASK_NAME, 
-      classOf[ScalaDoc],
-      new Action[ScalaDoc]:
-        override def execute(scalaDoc: ScalaDoc): Unit =
-          scalaDoc.setDescription("Generates Scaladoc for the main source code.")
-          scalaDoc.setGroup(JavaBasePlugin.DOCUMENTATION_GROUP)
-    )
+    if isCreate then
+      project.getTasks.register(
+        Original.SCALA_DOC_TASK_NAME, 
+        classOf[ScalaDoc],
+        new Action[ScalaDoc]:
+          override def execute(scalaDoc: ScalaDoc): Unit =
+            scalaDoc.setDescription("Generates Scaladoc for the main source code.")
+            scalaDoc.setGroup(JavaBasePlugin.DOCUMENTATION_GROUP)
+      )
     
